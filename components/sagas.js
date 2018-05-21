244296bd33d9f19a720315eae9414d490d3cb718
maxefi <maxefi@mail.ru>
2018-05-21 12:19:23 +0300

    test installing SDK

--- components/sagas.js
+++ components/sagas.js
@@ -1,14 +1,18 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import BitGoJS from 'bitgo';
+import { put, takeLatest } from 'redux-saga/effects';
+import { accessToken } from '../helpers/stubs';
 import * as Actions from './actions';
-import { authenticate, setAuthorizationHeader, unlock } from './service';
 
 export function* login() {
     try {
         yield put(Actions.loading({}));
-        const authenticateRes = yield call(authenticate);
-        const { data } = authenticateRes;
-        yield call(setAuthorizationHeader, data.access_token);
-        yield call(unlock);
+        const bitgo = new BitGoJS.BitGo({ env: 'test', accessToken });
+        const authenticateRes = yield call(bitgo.session({}));
+        console.log('login authenticateRes: ', authenticateRes);
+        // const authenticateRes = yield call(authenticate);
+        // const { data } = authenticateRes;
+        // yield call(setAuthorizationHeader, data.access_token);
+        // yield call(unlock);
         yield put(Actions.loaded({ data }));
     } catch (err) {
         yield put(Actions.failed({ err: err.toString() }));
--- helpers/stubs.js
+++ helpers/stubs.js
@@ -1,3 +1,5 @@
+export const accessToken = 'U5P3G6GJBJPBDJBY7BOGXKJPT2ZIV42LMYB5WTCUAXGAQ422JJDA';
+
 export const loginStub = {
     email: 'mefimov@distillery.com',
     password: 'qetu135',
